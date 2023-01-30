1179ba0d2f696987f9bfcd34cb75864d19d1b127
HC <h.chiadmi@deal.fr>
2023-01-30 14:35:31 +0100

    Created routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayersListComponent } from './components/players-list/players-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'players', pathMatch: 'full' },
+  { path: 'players', component: PlayersListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
